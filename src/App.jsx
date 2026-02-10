bdf705f6e22253af5eac1f0808070af3f9d19f5f
Saranya Gopinath <142643776+SaranyaGopinath25@users.noreply.github.com>
2026-02-10 13:52:22 +0100

    Responsive style changes done

--- src/App.jsx
+++ src/App.jsx
@@ -29,11 +29,11 @@ function App() {
   // Show product list
   return (
     <div className="grid place-items-center py-20">
-      <h1 className="text-5xl italic text-gray-500 mb-16">
+      <h1 className="lg:text-5xl md:text-4xl text-3xl italic text-gray-500 mb-16 px-10 text-center">
         Trend Alerts: Must-Have Outfits of the Season
       </h1>
       <ShoppingCart />
-      <div className="grid grid-cols-3 place-items-start gap-10">
+      <div className="grid xl:grid-cols-3 lg:grid-cols-2 place-items-start gap-10 xl:px-6 px-10">
         {allItems?.map((item) => {
           return(
             <CartItem 
